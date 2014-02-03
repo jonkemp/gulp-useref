b58def9eb4b189985398ea7bc077126d330cf349
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-02-03 16:47:30 -0500

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    mocha = require('gulp-mocha');
+
+var paths = {
+    scripts: ['./*.js', '!./gulpfile.js']
+};
+
+gulp.task('lint', function() {
+    return gulp.src(paths.scripts)
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('test', function() {
+    return gulp.src('./test/*.js')
+        .pipe(mocha());
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.scripts, ['lint', 'test']);
+});
+
+gulp.task('default', ['lint', 'test', 'watch']);
