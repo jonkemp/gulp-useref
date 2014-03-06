ee1434aad7f93c6f0fea9a30ccc36d611220aa0f
Jonathan Kemp <jonathan.kemp@cellularsales.com>
2014-03-05 23:27:00 -0500

    Concat and add js and css to stream after parsing build scripts with useref.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+'use strict';
 var gutil = require('gulp-util');
 var through = require('through2');
 var useref = require('useref');
+var path = require('path');
+var fs = require('fs');
 
 module.exports = function(){
     return through.obj(function (file, enc, cb) {
@@ -14,8 +17,40 @@ module.exports = function(){
             return cb();
         }
 
+        var output = useref(file.contents.toString());
+        var html = output[0];
+        var assets = output[1];
+
+        ['css', 'js'].forEach(function (type) {
+            var files = assets[type];
+            if (files) {
+                Object.keys(files).forEach(function (name) {
+                    var buffer = [];
+                    var filepaths = files[name];
+                    filepaths.forEach(function (filepath) {
+                        filepath = path.join(file.base, filepath);
+                        try {
+                            buffer.push(fs.readFileSync(filepath));
+                        } catch (err) {
+                            this.emit('error', new gutil.PluginError('gulp-useref', err));
+                        }
+                    }.bind(this));
+
+                    var joinedFile = new gutil.File({
+                        cwd: file.cwd,
+                        base: file.base,
+                        path: path.join(file.base, name),
+                        contents: new Buffer(buffer.join(gutil.linefeed))
+                    });
+
+                    this.push(joinedFile);
+
+                }.bind(this));
+            }
+        }.bind(this));
+
         try {
-            file.contents = new Buffer(useref(file.contents.toString())[0]);
+            file.contents = new Buffer(html);
         } catch (err) {
             this.emit('error', new gutil.PluginError('gulp-useref', err));
         }
