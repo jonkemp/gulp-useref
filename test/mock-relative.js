23e314342d9d0b1f63aecd98f1c7db890eb4a24f
Jonathan Kemp <kempdogg@gmail.com>
2018-10-30 22:48:16 -0400

    Update to gulp v4. (#255)

--- test/mock-relative.js
+++ test/mock-relative.js
@@ -2,60 +2,63 @@
 /* global describe, it */
 
 'use strict';
-var should = require('should');
-var path = require('path');
+var should = require('should'),
+    path = require('path'),
+    once = require('async-once');
 
 describe('relative files', function () {
     this.timeout(5000);
 
     it('should handle relative files', function (done) {
-        var gulp = require('gulp');
-        var mockGulpDest = require('mock-gulp-dest')(gulp);
+        var seriesFn,
+            gulp = require('gulp'),
+            mockGulpDest = require('mock-gulp-dest')(gulp),
+            useref = require('../index');
 
-        var useref = require('../index');
-
-        gulp.task('relative', function () {
+        function relative() {
             return gulp.src('test/fixtures/relative/**/*.html')
                 .pipe(useref())
                 .pipe(gulp.dest('fixtures/relative/styles', { cwd: 'test' }));
-        });
+        }
 
-        gulp.start('relative')
-            .once('stop', function () {
-                mockGulpDest.cwd().should.equal(__dirname);
-                mockGulpDest.basePath().should.equal(path.join(__dirname, 'fixtures/relative/styles'));
-                mockGulpDest.assertDestContains([
-                    'bundle.css'
-                ]);
+        seriesFn = gulp.series(relative, once(function () {
+            mockGulpDest.cwd().should.equal(__dirname);
+            mockGulpDest.basePath().should.equal(path.join(__dirname, 'fixtures/relative/styles'));
+            mockGulpDest.assertDestContains([
+                'bundle.css'
+            ]);
 
-                done();
-            });
+            done();
+        }));
+
+        seriesFn();
     });
 
     it('should handle relative files when base is set', function (done) {
-        var gulp = require('gulp');
-        var mockGulpDest = require('mock-gulp-dest')(gulp);
+        var seriesFn,
+            gulp = require('gulp'),
+            mockGulpDest = require('mock-gulp-dest')(gulp),
+            useref = require('../index');
 
-        var useref = require('../index');
-
-        gulp.task('relativeBase', function () {
+        function relativeBase() {
             return gulp.src(['test/fixtures/templates1/**/*.html', 'test/fixtures/templates2/**/*.html'])
                 .pipe(useref({
                     searchPath: 'test/fixtures',
                     base: 'test/fixtures'
                 }))
                 .pipe(gulp.dest('test/dist'));
-        });
+        }
 
-        gulp.start('relativeBase')
-            .once('stop', function () {
-                mockGulpDest.basePath().should.equal(path.join(__dirname, 'dist'));
-                mockGulpDest.assertDestContains([
-                    'css/bundle.css',
-                    'css/combined.css'
-                ]);
+        seriesFn = gulp.series(relativeBase, once(function () {
+            mockGulpDest.basePath().should.equal(path.join(__dirname, 'dist'));
+            mockGulpDest.assertDestContains([
+                'css/bundle.css',
+                'css/combined.css'
+            ]);
 
-                done();
-            });
+            done();
+        }));
+
+        seriesFn();
     });
 });
